224f7e25319664602a1aa22afe211390e97847c9
sagorpaul142 <sagorpaulcse@gmail.com>
2023-09-03 23:31:40 +0600

    tailwind css install and configure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<div class="container mx-auto">
+    <h1 class="text-5xl mt-4">Welcome to Home Page</h1>
+</div>
