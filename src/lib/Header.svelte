52170867febe793f4e9cb90abb36787dd667e1ac
sagorpaul142 <sagorpaulcse@gmail.com>
2023-09-04 11:09:18 +0600

    layout and navabr add

---
+++ src/lib/Header.svelte
@@ -0,0 +1,28 @@
+<nav class="bg-white dark:bg-gray-900 w-full border-b border-gray-200 dark:border-gray-600">
+    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-3">
+        <a href="/" class="flex items-center">
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Logo</span>
+        </a>
+        <div class="items-center justify-between flex w-auto " id="navbar-sticky">
+            <ul class="flex p-0 font-medium border-gray-100 rounded-lg  flex-row space-x-8 mt-0 border-0 bg-white dark:bg-gray-900 dark:border-gray-700">
+                <li>
+                    <a href="#"
+                       class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 bg-transparent "
+                       aria-current="page">Layout</a>
+                </li>
+                <li>
+                    <a href="#"
+                       class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Chart
+                    </a>
+                </li>
+                <li>
+                    <a href="#"
+                       class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        Map
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
