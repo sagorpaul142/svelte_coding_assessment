4f191d6177c9bc9eb111f1b31b0e20fcf7733ea0
sagorpaul142 <sagorpaulcse@gmail.com>
2023-09-04 16:03:58 +0600

    map page add and map show in page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -22,7 +22,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#"
+                    <a href="/map" class:active={$page.route.id === '/map'}
                        class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                         Map
                     </a>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,69 @@
+<script>
+  import { onMount } from 'svelte';
+  import 'ol/ol.css';
+  import Map from 'ol/Map';
+  import View from 'ol/View';
+  import TileLayer from 'ol/layer/Tile';
+  import OSM from 'ol/source/OSM';
+  import Geolocation from 'ol/Geolocation';
+  import Overlay from 'ol/Overlay';
+
+  let geolocation;
+
+  onMount(() => {
+    const map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+
+    geolocation = new Geolocation({
+      tracking: true,
+      trackingOptions: {
+        enableHighAccuracy: true,
+      },
+      projection: map.getView().getProjection(),
+    });
+
+    const marker = new Overlay({
+      element: document.getElementById('marker'),
+      positioning: 'center-center',
+      stopEvent: false,
+    });
+    map.addOverlay(marker);
+
+    geolocation.on('change:position', () => {
+      const coordinates = geolocation.getPosition();
+      marker.setPosition(coordinates);
+      map.getView().setCenter(coordinates);
+    });
+
+    geolocation.setTracking(true);
+  });
+</script>
+
+<style>
+  #map {
+    width: 100%;
+    height: calc(100vh - 57px);
+  }
+
+  #marker {
+    width: 20px;
+    height: 20px;
+    background-color: blue;
+    border-radius: 50%;
+  }
+</style>
+
+<main>
+  <div id="map"></div>
+  <div id="marker"></div>
+</main>
